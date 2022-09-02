f58904b69da1c0e854d1f219edbf0190ae64e8d7
KINE-M <a.m.cassandre.k@gmail.com>
2022-09-02 21:12:40 +0900

    fix: #8 Google Map API vite-env.d.tsの修正

--- frontend/app/src/vite-env.d.ts
+++ frontend/app/src/vite-env.d.ts
@@ -1,5 +1,8 @@
 /// <reference types="vite/client" />
-
-type ImportMetaEnv = {
+interface ImportMetaEnv {
   readonly VITE_GOOGLE_MAP_API_KEY: string;
-};
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
