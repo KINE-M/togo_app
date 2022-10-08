a4d2e3e4fa4e22759d4167a3894a0e48cf13a472
KINE-M <a.m.cassandre.k@gmail.com>
2022-10-08 23:13:32 +0900

    fix: #17 FeaturedPost, Posts, AllPosts, Home

--- frontend/app/src/components/Home.tsx
+++ frontend/app/src/components/Home.tsx
@@ -1,26 +1,16 @@
-import { useNavigate } from 'react-router-dom';
-import { Box, Button, Container, Paper } from '@mui/material';
-import Posts from './post/Posts';
+import { Container, Paper } from '@mui/material';
 import MyTogoList from './togo/MyTogoList';
+import Posts from './post/Posts';
 
-const Home = () => {
-  const navigate = useNavigate();
-
-  return (
-    <Container maxWidth="lg" sx={{ pt: 2 }}>
-      <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column', position: 'relative' }}>
-        <MyTogoList />
-      </Paper>
-      <Paper sx={{ mt: 2 }}>
-        <Posts count={2} />
-        <Box sx={{ pb: 1, display: 'flex', justifyContent: 'center' }}>
-          <Button variant="contained" onClick={() => navigate('/posts')}>
-            もっとみる
-          </Button>
-        </Box>
-      </Paper>
-    </Container>
-  );
-};
+const Home = () => (
+  <Container maxWidth="lg" sx={{ pt: 2 }}>
+    <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column', position: 'relative' }}>
+      <MyTogoList />
+    </Paper>
+    <Paper sx={{ mt: 2 }}>
+      <Posts count={2} />
+    </Paper>
+  </Container>
+);
 
 export default Home;
--- frontend/app/src/components/post/FeaturedPost.tsx
+++ frontend/app/src/components/post/FeaturedPost.tsx
@@ -21,42 +21,36 @@ const FeaturedPost: React.FC<FeaturedPostProps> = ({ post }) => {
 
   return (
     <Grid item xs={12} md={6}>
-      {post.togo.id !== undefined && (
-        <CardActionArea onClick={() => navigate(`/posts/${Number(post.togo.id)}`)}>
-          <Card sx={{ display: 'flex' }}>
-            <CardContent sx={{ flex: 1 }}>
-              <Typography component="h5" variant="h5" sx={{ fontSize: '18px', fontWeight: 'bold' }}>
-                {post.title}
-              </Typography>
-              <Box component="div" sx={{ display: 'inline' }}>
-                {post.togo.location}
-              </Box>
-              <Box component="div" sx={{ display: 'inline', ml: 1 }}>
-                <Chip label={post.togo.tag} variant="outlined" size="small" />
-              </Box>
-              <Box component="div" sx={{ display: 'flex' }}>
-                投稿日：{post.publishedAt}
-              </Box>
-              <Typography
-                variant="inherit"
-                paragraph
-                sx={{ height: 40, overflow: 'hidden', mt: 2 }}
-              >
-                {post.description}
-              </Typography>
-              <Typography variant="subtitle1" color="primary">
-                Continue reading...
-              </Typography>
-            </CardContent>
-            <CardMedia
-              component="img"
-              sx={{ width: 160, display: { xs: 'none', sm: 'block' } }}
-              image={post.image}
-              alt={post.title}
-            />
-          </Card>
-        </CardActionArea>
-      )}
+      <CardActionArea onClick={() => navigate(`/posts/${Number(post.togo.id)}`)}>
+        <Card sx={{ display: 'flex' }}>
+          <CardContent sx={{ flex: 1 }}>
+            <Typography component="h5" variant="h5" sx={{ fontSize: '18px', fontWeight: 'bold' }}>
+              {post.title}
+            </Typography>
+            <Box component="div" sx={{ display: 'inline' }}>
+              {post.togo.location}
+            </Box>
+            <Box component="div" sx={{ display: 'inline', ml: 1 }}>
+              <Chip label={post.togo.tag} variant="outlined" size="small" />
+            </Box>
+            <Box component="div" sx={{ display: 'flex' }}>
+              投稿日：{post.publishedAt}
+            </Box>
+            <Typography variant="inherit" paragraph sx={{ height: 40, overflow: 'hidden', mt: 2 }}>
+              {post.description}
+            </Typography>
+            <Typography variant="subtitle1" color="primary">
+              Continue reading...
+            </Typography>
+          </CardContent>
+          <CardMedia
+            component="img"
+            sx={{ width: 160, display: { xs: 'none', sm: 'block' } }}
+            image={post.image}
+            alt={post.title}
+          />
+        </Card>
+      </CardActionArea>
     </Grid>
   );
 };
