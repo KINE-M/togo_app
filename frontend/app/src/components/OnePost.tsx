3f859f1317976776675242ad53b510575c90ce2a
KINE-M <a.m.cassandre.k@gmail.com>
2022-10-13 19:20:32 +0900

    feat: #18 投稿詳細ページ

--- frontend/app/src/components/OnePost.tsx
+++ frontend/app/src/components/OnePost.tsx
@@ -1,12 +1,13 @@
 import { Container } from '@mui/material';
 import { useParams } from 'react-router-dom';
+import PostDetail from './post/PostDetail';
 
 const OnePost = () => {
   const { id } = useParams();
 
   return (
     <Container maxWidth="lg" sx={{ pt: 2 }}>
-      One Post {id}
+      <PostDetail togoId={Number(id)} />
     </Container>
   );
 };
